473a19eb24a8e13bf1e8ae0d7524e7cfd57e5875
EphraimHaber <ephraimxyz@gmail.com>
2023-12-04 17:15:42 +0200

    improved performance for day 1

--- day-1/main.py
+++ day-1/main.py
@@ -1,3 +1,6 @@
+from typing import Generator
+
+
 def get_first_last_digits(config: str) -> int:
     digits = "".join(c for c in config if c.isdigit())
     first_and_last = f'{digits[0]}{digits[-1]}'
@@ -7,5 +10,6 @@ def get_first_last_digits(config: str) -> int:
 if __name__ == '__main__':
     with open('puzzle.txt', 'r') as file:
         content = file.read().split('\n')
-        res = sum([get_first_last_digits(calibration) for calibration in content])
+        number_generator: Generator[int, None, None] = (get_first_last_digits(calibration) for calibration in content)
+        res = sum(number_generator)
         print(res)
