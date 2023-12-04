f4f011dab88a4343436d57d09928194dc13de4a2
EphraimHaber <ephraimxyz@gmail.com>
2023-12-04 16:56:36 +0200

    solved day 1

---
+++ day-1/main.py
@@ -0,0 +1,11 @@
+def get_first_last_digits(config: str) -> int:
+    digits = "".join(c for c in config if c.isdigit())
+    first_and_last = f'{digits[0]}{digits[-1]}'
+    return int(first_and_last)
+
+
+if __name__ == '__main__':
+    with open('puzzle.txt', 'r') as file:
+        content = file.read().split('\n')
+        res = sum([get_first_last_digits(calibration) for calibration in content])
+        print(res)
